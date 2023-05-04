7b5cb8943baa61dbf23c253e552299493ad9316a
Alen John <alensden@gmail.com>
2023-05-04 12:08:09 -0300

    server configured

--- server/index.js
+++ server/index.js
@@ -8,34 +8,49 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { error } from "console";
 
 /**
  * Configuration of middleware
  */
 
-const __filename = fileURLToPath(import.meta.url); // only because we used ES6 imports 
+const __filename = fileURLToPath(import.meta.url); // only because we used ES6 imports
 const __dirname = path.dirname(__filename); // To enable ES6 Imports
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP 
-app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}))
+app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /**
  * Configuration of File Storage
  */
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function(req, file, cb){
-        cb(null, file.originalname);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
+
+/**
+ * Setting Mongoose
+ */
+const PORT = process.env.PORT || 6001;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server started at : ${PORT}`));
+  })
+  .catch((error) => console.log(`error occurred : ${error}`));
