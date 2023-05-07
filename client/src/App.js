73fba47b32942c0b82b691d523b20b556378bc18
Alen John <alensden@gmail.com>
2023-05-07 12:03:02 -0300

    state mgmt completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,56 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  /**
+   * useMemo allows you to memoize the result of a function call,
+   * which means that the function is only executed when necessary,
+   * and the cached result is returned on subsequent renders.
+   * This can be useful when you have a function that is called frequently or has a
+   * high computational cost, and you want to avoid unnecessary re-execution of that function.
+   *
+   * In the context of Redux, useMemo can be used to optimize the mapping of Redux state
+   * to props in a React component. When you use useSelector to select data from the Redux store,
+   * the component will re-render whenever the selected data changes, even if the component doesn't
+   * actually use that data. This can cause unnecessary re-renders and reduce performance.
+   * By wrapping the selector with useMemo, you can memoize the result and prevent the component
+   * from re-rendering when the selected data hasn't actually changed.
+   */
+
+  /**
+   * In this code, [mode] is a dependency array passed to the useMemo hook.
+   * This means that the memoized value (theme) will only be recomputed if the value of mode changes.
+   * The createTheme function is likely creating a theme object based on the value of mode.
+   * By passing [mode] as a dependency array, the useMemo hook will ensure that the theme object is only
+   * re-created when the mode value changes. This can help optimize the performance of the component
+   * by avoiding unnecessary re-creation of the theme object on every render.
+   * For example, let's say that mode can have two possible values: "light" and "dark".
+   * If mode is initially set to "light", the useMemo hook will create a theme object for the
+   * "light" mode. If mode then changes to "dark", the useMemo hook will re-compute the theme object
+   * for the "dark" mode. If mode remains "dark", the theme object will not be re-computed on subsequent renders unless mode changes again.
+   * By specifying the dependency array for useMemo, you can control when the memoized value should be
+   * re-computed based on changes to the dependencies. This can help to optimize the performance of your application.
+   */
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { configureStore } from "@reduxjs/toolkit";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+/**
+ * This is a code block that configures and creates a Redux store using the configureStore
+ * function from the @reduxjs/toolkit package, which provides a simplified interface for
+ * creating and working with Redux stores.
+ * The store is created with a persisted reducer, meaning that the application
+ * state will be persisted across sessions using the redux-persist package.
+ * The persistReducer function takes in a configuration object which includes
+ *  a key property for the storage key, a storage property to specify the storage
+ * engine, and a version property to help manage migrations.
+ * In addition, configureStore is passed a middleware function that is used to
+ * customize the behavior of the Redux store's middleware. Here, it uses the
+ * getDefaultMiddleware function to retrieve the default middleware, and passes
+ * it an object with a serializableCheck property to specify which actions should
+ * be ignored by the default serializable action check. The ignored actions are the
+ * ones listed under the ignoredActions array: FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, and REGISTER.
+ * Finally, the Redux store is exported as store and can be used in the application to manage global state.
+ */
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
-
