e7315e2e75ce171bcadf2cd7c39261bc860c6523
alensden2 <alensden@gmail.com>
2023-05-07 12:10:49 -0300

    reconfigured user name in git --alensden2

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { createTheme } from "@mui/material";
 import { themeSettings } from "theme";
 
 function App() {
